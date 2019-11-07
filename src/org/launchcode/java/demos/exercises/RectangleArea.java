1778ca0e639a6f148bd58d63fc262a9b55f540b9
suhachakka <suhasinichakka@gmail.com>
2019-11-07 10:18:22 -0600

    finished exercises

---
+++ src/org/launchcode/java/demos/exercises/RectangleArea.java
@@ -0,0 +1,16 @@
+package src.org.launchcode.java.demos.exercises;
+
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Length of a rectangle:");
+        int length = input.nextInt();
+        System.out.println("Enter Width of a rectangle");
+        int width = input.nextInt();
+        int area = length * width;
+        System.out.println("Area of rectangle with length is "+length+ " and width is "+width+ " = "+area);
+
+    }
+}
